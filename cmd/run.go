c922e1118753ef7304d0119c9ad737e9d8eea84f
Aleksandar Zizovic <42997751+z1z0v1c@users.noreply.github.com>
2025-06-17 15:47:46 +0200

    Execute ls subcommand

--- cmd/run.go
+++ cmd/run.go
@@ -2,14 +2,26 @@ package cmd
 
 import (
 	"fmt"
+	"os/exec"
 
 	"github.com/spf13/cobra"
 )
 
 var run = &cobra.Command{
-  Use: "run",
-  Short: "Equivalent to Docker run subcommand",
-  Run: func(cmd *cobra.Command, args []string) {
-    fmt.Println("Run subcommand is called.")
-  },  
+	Use:   "run",
+	Short: "Equivalent to the docker run subcommand",
+	Run:   Run,
+}
+
+func Run(cmd *cobra.Command, args []string) {
+	c := exec.Command("ls", "-l")
+
+	// Capture output
+	output, err := c.CombinedOutput()
+	if err != nil {
+		fmt.Printf("Error executing command: %v\n", err)
+		return
+	}
+
+	fmt.Printf("%s\n", output)
 }
