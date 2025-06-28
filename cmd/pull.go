b523ba928d1b44e5769be6e180b9c4a494d033e3
Aleksandar Zizovic <42997751+z1z0v1c@users.noreply.github.com>
2025-06-28 14:54:22 +0200

    Authenticate for the repository

--- cmd/pull.go
+++ cmd/pull.go
@@ -1,7 +1,10 @@
 package cmd
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/spf13/cobra"
 )
@@ -28,5 +31,36 @@ func Pull(c *cobra.Command, args []string) {
 	image := args[0]
 	repository := "library/" + image
 
-	log.Printf("Pulling %s image from the %s repository in %s registry.", image, repository, registry)
+	log.Printf("Pulling %q image from the %q repository in %q registry...", image, repository, registry)
+
+	log.Printf("Authenticating for the %q repository...", repository)
+
+	token, err := authenticate(repository)
+	if err != nil {
+		log.Fatalf("authentication failed: %v", err)
+	}
+
+	log.Printf("Token: %s", token)
+}
+
+func authenticate(repository string) (string, error) {
+	// For Docker Hub, we need to get a token from auth.docker.io
+	authURL := fmt.Sprintf(auth, repository)
+
+	resp, err := http.Get(authURL)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("authentication failed with status: %d", resp.StatusCode)
+	}
+
+	var authResp AuthResponse
+	if err := json.NewDecoder(resp.Body).Decode(&authResp); err != nil {
+		return "", err
+	}
+
+	return authResp.Token, nil
 }
