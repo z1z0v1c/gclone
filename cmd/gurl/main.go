2e1e00a7eaaa20aa0fffd7c017003251da58a561
Aleksandar Zizovic <42997751+z1z0v1c@users.noreply.github.com>
2025-08-09 23:31:29 +0200

    [gurl] Move main func to internal package

--- cmd/gurl/main.go
+++ cmd/gurl/main.go
@@ -1,80 +1,24 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"net"
-	"net/url"
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/z1z0v1c/gclone/internal/gurl"
 )
 
-// gurl is the root Cobra command for the gURL
-var gurl = &cobra.Command{
+// root is the root Cobra command for gURL
+var root = &cobra.Command{
 	Use:   "gurl command [flags]",
 	Short: "Simple cURL clone",
 	Args:  cobra.ExactArgs(1),
-	Run: func(c *cobra.Command, args []string) {
-		url, err := url.Parse(args[0])
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Invalid url: %v.\n", err)
-			os.Exit(1)
-		}
-
-		protocol := url.Scheme
-		if protocol != "http" {
-			fmt.Fprintf(os.Stderr, "Invalid protocol. Only http is supported.\n")
-			os.Exit(1)
-		}
-
-		host := url.Hostname()
-		port := url.Port()
-		if port == "" {
-			port = "80"
-		}
-
-		path := url.Path
-		if path == "" {
-			path = "/"
-		}
-
-		addr := net.JoinHostPort(host, port)
-		conn, err := net.Dial("tcp", addr)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error connecting to server: %v\n", err)
-			os.Exit(1)
-		}
-		defer conn.Close()
-
-		req := fmt.Sprintf("GET %s HTTP/1.1\r\n", path)
-		req += fmt.Sprintf("Host: %s\r\n", host)
-		req += "Accept: */*\r\n"
-		req += "Connection: close\r\n"
-		req += "\r\n"
-
-		fmt.Printf("Sending request %s", req)
-
-		_, err = conn.Write([]byte(req))
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error sending request: %v\n", err)
-			os.Exit(1)
-		}
-
-		reader := bufio.NewReader(conn)
-		for {
-			line, err := reader.ReadString('\n')
-			if err != nil {
-				break
-			}
-			fmt.Print(line)
-		}
-	},
+	Run:   gurl.Gurl,
 }
 
 func main() {
-	if err := gurl.Execute(); err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+	if err := root.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
 }
---
+++ internal/gurl/gurl.go
@@ -0,0 +1,67 @@
+package gurl
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"net/url"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func Gurl(c *cobra.Command, args []string) {
+	url, err := url.Parse(args[0])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Invalid url: %v.\n", err)
+		os.Exit(1)
+	}
+
+	protocol := url.Scheme
+	if protocol != "http" {
+		fmt.Fprintf(os.Stderr, "Invalid protocol. Only http is supported.\n")
+		os.Exit(1)
+	}
+
+	host := url.Hostname()
+	port := url.Port()
+	if port == "" {
+		port = "80"
+	}
+
+	path := url.Path
+	if path == "" {
+		path = "/"
+	}
+
+	addr := net.JoinHostPort(host, port)
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error connecting to server: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	req := fmt.Sprintf("GET %s HTTP/1.1\r\n", path)
+	req += fmt.Sprintf("Host: %s\r\n", host)
+	req += "Accept: */*\r\n"
+	req += "Connection: close\r\n"
+	req += "\r\n"
+
+	fmt.Printf("Sending request %s", req)
+
+	_, err = conn.Write([]byte(req))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error sending request: %v\n", err)
+		os.Exit(1)
+	}
+
+	reader := bufio.NewReader(conn)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+		fmt.Print(line)
+	}
+}
